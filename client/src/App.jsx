0910b7c168a84ee9b264ad088143d9ea536565f8
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2022-07-12 19:29:16 +0530

    stripe payment (error fix just by reloading node app)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import {
   Route,
   Navigate
 } from "react-router-dom";
+import Success from "./pages/Success";
 
 const App = () => {
   const user = true;  //just for temporary purpose
@@ -27,6 +28,9 @@ const App = () => {
         </Route>
 
         <Route path="/cart" element={<Cart />}>
+        </Route>
+
+        <Route path="/success" element={<Success />}>
         </Route>
 
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />}>
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -7,6 +7,8 @@ import Navbar from "../components/Navbar";
 import { mobile } from "../responsive";
 import StripeCheckout from "react-stripe-checkout";
 import { useEffect, useState } from "react";
+import { userRequest } from "../requestMethods";
+import { useNavigate } from 'react-router-dom';
 
 const KEY = process.env.REACT_APP_STRIPE;
 
@@ -160,12 +162,29 @@ const Button = styled.button`
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
   const [stripeToken, setStripeToken] = useState(null);
+  const navigate = useNavigate();
 
   const onToken = (token) => {
     setStripeToken(token);
   };
 
-  console.log(stripeToken);
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await userRequest.post("/checkout/payment", {
+          tokenId: stripeToken.id,
+          amount: 500,
+        });
+        navigate.push("/success", {
+          stripeData: res.data,
+          products: cart,
+        });
+      } catch { }
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken, cart.total, navigate]);
+
+  //console.log(stripeToken);
   return (
     <Container>
       <Navbar />
